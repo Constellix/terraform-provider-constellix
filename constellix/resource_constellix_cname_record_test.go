0efd2c3db559f66bc18f62472ba27bd5e94f904e
Jonathan Cox <coxjonc@gmail.com>
2020-09-30 15:03:36 -0400

    Add geolocation to Update test

--- constellix/resource_constellix_cname_record_test.go
+++ constellix/resource_constellix_cname_record_test.go
@@ -79,6 +79,11 @@ func testAccCheckConstellixCNameConfig_basic(ttl int) string {
 		name = "tempcnamerecord"
 		ttl = "%d"
 		note = "Practice record naptr"
+
+	  geo_location {
+		geo_ip_proximity = 2092
+		geo_ip_failover  = true
+	  }
 		record_option = "failover"
 	    record_failover_values  {
 			     value = "a."
@@ -92,8 +97,6 @@ func testAccCheckConstellixCNameConfig_basic(ttl int) string {
 			  }
 			   record_failover_failover_type = 1
 			   record_failover_disable_flag = "false"
-			 
-			
 	}
 	`, ttl)
 }
