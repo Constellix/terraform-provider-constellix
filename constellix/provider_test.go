b5b66095813c214489d239478286a1410370ba3b
Junaid Ahmad <junaid.ahmad@digicert.com>
2024-07-25 11:17:56 +0100

    refactor: Check both old and new envs for API key and secret.

--- constellix/provider_test.go
+++ constellix/provider_test.go
@@ -31,11 +31,11 @@ func TestProvider_impl(t *testing.T) {
 func testAccPreCheck(t *testing.T) {
 	// We will use this function later on to make sure our test environment is valid.
 	// For example, you can make sure here that some environment variables are set.
-	if v := os.Getenv("apikey"); v == "" {
+	if os.Getenv("apikey") == "" && os.Getenv("CONSTELLIX_API_KEY") == "" {
 		t.Fatal("API KEY env variable must be set for acceptance tests")
 	}
 
-	if v := os.Getenv("secretkey"); v == "" {
+	if os.Getenv("secretkey") == "" && os.Getenv("CONSTELLIX_SECRET_KEY") == "" {
 		t.Fatal("SECRET KEY env variable must be set for acceptance tests")
 	}
 }
